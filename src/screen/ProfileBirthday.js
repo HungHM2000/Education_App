ff1538c978cc3cac19566980fbca8692f89f3d75
quocbaooo <bao.ngo@ntq-solution.com.vn>
2022-07-14 17:31:41 +0700

    new commit

--- src/screen/ProfileBirthday.js
+++ src/screen/ProfileBirthday.js
@@ -1,9 +1,22 @@
 import { View, Text, SafeAreaView, TextInput, Button } from 'react-native'
 import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/dist/Fontisto';
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
 const ProfileBirthday = () => {
+    
+    const [date, setDate] = useState(new Date())
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    const [text, setText] = useState('');
+
   
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    }
+
     return ( 
         <SafeAreaView>
            
