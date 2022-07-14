12f650333efef97a5766488200d7f17da30593d1
daibo1411 <109138653+daibo1411@users.noreply.github.com>
2022-07-14 12:35:21 +0700

    ok

--- src/component/detail/YoutubeComponent.js
+++ src/component/detail/YoutubeComponent.js
@@ -1,6 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useState, useCallback, useRef} from 'react';
-import YoutubePlayer from 'react-native-youtube-iframe';
 
 export default function YoutubeComponent() {
   return (
