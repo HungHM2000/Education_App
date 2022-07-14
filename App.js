0549499972f1b3798680a21ea0f3a74db31fb420
daibo1411 <109138653+daibo1411@users.noreply.github.com>
2022-07-14 11:04:38 +0700

    ok

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import YoutubeComponent from './src/component/detail/YoutubeComponent';
+import DetailScreen from './src/screen/DetailScreen';
 
 export default function App() {
   return (
     <View>
-      <Text>App</Text>
+      <YoutubeComponent />
     </View>
   );
 }
---
+++ src/component/detail/YoutubeComponent.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useCallback, useRef} from 'react';
+import YoutubePlayer from 'react-native-youtube-iframe';
+
+export default function YoutubeComponent() {
+  return (
+    <View>
+      <Text>YoutubeComponent</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
